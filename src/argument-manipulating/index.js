64d99ca4c6c868bb3bae160e533cb9f0896c1d2f
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-06-15 00:38:10 +0430

    feat: add argument-manipulating.

---
+++ src/argument-manipulating/index.js
@@ -0,0 +1,39 @@
+function increase(fn) {
+    return function increased(...args) {
+      return fn(...args.map(arg => arg + 1));
+    }
+}
+
+function spread(fn) {
+  return function spreaded(args) {
+    return fn(...args);
+  }
+}
+
+function flip(fn) {
+  return function flipped(x, y, ...args) {
+    return fn(y, x, ...args);
+  }
+}
+
+function reverse(fn) {
+  return function reversed(...args) {
+    return fn(...args.reverse());
+  }
+}
+
+function f(...args) {
+  console.log(args);
+}
+
+const g = reverse(f);
+g(1, 2, 3, 4, 5);
+
+const h = flip(f);
+h(1, 2, 3, 4, 5);
+
+const j = increase(f);
+j(1, 2, 3, 4, 5);
+
+const k = spread(f);
+k([1,2,3,4,5]);
