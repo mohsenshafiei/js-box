d2d346e18a52356a8d80333e9e6dcd9594e2672e
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-06-15 01:33:50 +0430

    feat: add global pipe and composition of the functions.

--- src/function-composition/index.js
+++ src/function-composition/index.js
@@ -1,14 +1,29 @@
-function compose(fn1, fn2) {
+function compose2(fn1, fn2) {
   return function composed(...args) {
     return fn2(fn1(...args));
   }
 }
 
-const increment = (x) => x + 1;
-const double = (x) => x * 2;
+function compose(...fns) {
+  return function(result) {
+    for (let i = fns.length - 1  ; i >= 0 ; i--) {
+      result = fns[i](result);
+    }
+    return result;
+  }
+}
 
-const f = compose(increment, double);
-const g = compose(double, increment);
+
+
+const increment = (x) => x + 1;
+const decrement = (x) => x + 1;
+const double = (x) => x * 2;
+const half = (x) => x / 2;
+
+const f = compose2(increment, double);
+const g = compose2(double, increment);
+const x = compose(decrement, half, increment, double);
 
 console.log(f(3));
 console.log(g(3));
+console.log(x(3));
--- src/function-pipe/index.js
+++ src/function-pipe/index.js
@@ -1,14 +1,27 @@
-function pipe(fn1, fn2) {
+function pipe2(fn1, fn2) {
   return function piped(...args) {
     return fn1(fn2(...args));
   }
 }
 
-const increment = (x) => x + 1;
-const double = (x) => x * 2;
+function pipe(...fns) {
+  return function(result) {
+    for (let i = 0 ; i < fns.length ; i++) {
+      result = fns[i](result);
+    }
+    return result;
+  }
+}
 
-const f = pipe(increment, double);
-const g = pipe(double, increment);
+const increment = (x) => x + 1;
+const decrement = (x) => x + 1;
+const double = (x) => x * 2;
+const half = (x) => x / 2;
+
+const f = pipe2(increment, double);
+const g = pipe2(double, increment);
+const x = pipe(double, increment, half, decrement);
 
 console.log(f(3));
 console.log(g(3));
+console.log(x(3));
