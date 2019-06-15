44d1fb0a2a901407c0551fe63b28349e6b1b4c06
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-06-15 08:44:09 +0430

    feat: add some impure closure to fully undestand the closure.

--- src/closure/index.js
+++ src/closure/index.js
@@ -18,6 +18,9 @@ function bar(x , y) {
   };
 }
 
+// This is some adhoc form of memoization
+// function get input and save output for that input
+// that cache is not observable in other parts of the system
 function baz(x , y) {
   let sum;
   return function () {
@@ -28,7 +31,6 @@ function baz(x , y) {
   };
 }
 
-
 const x = foo(3, 4);
 const y = bar(3, 4);
 const z = baz(3, 4);
@@ -36,3 +38,18 @@ const z = baz(3, 4);
 console.log(x());
 console.log(y());
 console.log(z());
+
+// the rule of closure is to get a function and give it a memory
+// NOTE: this is not pure - please make sure that to use closure in the right way
+function bax() {
+  var id = 0;
+  return function () {
+    return id++;
+  }
+}
+
+const t = bax();
+t(); // 0
+t(); // 1
+t(); // 2
+// so this is not pure and we don't have referential transparency
