270975f8e1a9b1ff567cc1b28b4297bfbc663f0d
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-06-15 09:37:41 +0430

    feat: add recursion to the project.

---
+++ src/recursion/index.js
@@ -0,0 +1,22 @@
+function summation(x, y, ...args) {
+  if (args.length === 0) return x + y;
+  return x + summation(y, ...args);
+}
+
+function multiplication(x, y, ...args) {
+  if (args.length === 0) return x * y
+  return x * multiplication(y, ...args);
+}
+
+function multiplication2(x, y, ...args) {
+  if (y === undefined) return x;
+  return multiplication(x * y, ...args);
+}
+
+const sum = summation(1,2,2,3,3,4,5);
+const mult = multiplication(1,2,2);
+const mult2 = multiplication(1,2,2);
+
+console.log(sum);
+console.log(mult);
+console.log(mult2);
